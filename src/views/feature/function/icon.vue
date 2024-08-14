6db6521292feb97c497d7c8f3cbcf36f411f9f98
Hooray Hu <304327508@qq.com>
2024-08-14 18:07:02 +0800

    SvgIcon 组件使用网络图片时增加加载和失败状态

--- src/views/feature/function/icon.vue
+++ src/views/feature/function/icon.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 definePage({
   meta: {
-    title: '图标',
+    title: 'pages.function.icon',
   },
 })
+
+const webImage = ref('https://picsum.photos/50')
+function change() {
+  webImage.value = ''
+  webImage.value = `https://picsum.photos/50?random=${Math.random()}`
+}
+
+const fm = new URL('@/assets/images/logo.png', import.meta.url).href
 
 const flip = ref<'horizontal' | 'vertical' | 'both'>()
 const rotate = ref(0)
 </script>
 
 <template>
-  <PageLayout navbar>
+  <PageLayout navbar navbar-start-side="back">
     <PageMain>
       <p>单色 Icon</p>
       <div>
@@ -30,6 +38,18 @@ const rotate = ref(0)
         <SvgIcon name="i-carbon:play-filled-alt" :size="48" :flip="flip" :rotate="rotate" />
         <SvgIcon name="i-carbon:pause-filled" :size="48" :flip="flip" :rotate="rotate" />
       </div>
+      <p>网络图片</p>
+      <SvgIcon :name="webImage" :size="48" :flip="flip" :rotate="rotate" />
+      <div class="space-x-2">
+        <HButton @click="change">
+          更改图片
+        </HButton>
+        <HButton @click="webImage = ''">
+          创建错误
+        </HButton>
+      </div>
+      <p>本地图片</p>
+      <SvgIcon :name="fm" :size="48" :flip="flip" :rotate="rotate" />
       <div>
         <p>翻转：</p>
         <van-radio-group v-model="flip" class="space-y-1">
